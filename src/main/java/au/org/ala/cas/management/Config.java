179ad200c3e6772c2d88cd20f3ec8590e4a606e0
Simon Bear <simon.bear@csiro.au>
2018-04-11 18:57:23 +1000

    Remove unnecessary logger

--- src/main/java/au/org/ala/cas/management/Config.java
+++ src/main/java/au/org/ala/cas/management/Config.java
@@ -1,7 +1,5 @@
 package au.org.ala.cas.management;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +8,9 @@ import org.springframework.session.data.redis.config.ConfigureRedisAction;
 @Configuration
 public class Config {
 
-    private static final Logger logger = LoggerFactory.getLogger(Config.class);
-
     @ConditionalOnProperty("spring.session.disable-redis-config-action")
     @Bean
     public ConfigureRedisAction configureRedisAction() {
-        logger.error("configureRedisAction()");
         return ConfigureRedisAction.NO_OP;
     }
 
